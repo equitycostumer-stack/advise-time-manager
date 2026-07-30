8aa3124dfe829123614eb25138648bd0565972e1
Equity Costumer <equitycostumer@gmail.com>
2026-07-30 16:12:43 -0500

    Actualizar URL de la API de producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://advise-time-manager-production.up.railway.app",
 });
 
 export default api;
